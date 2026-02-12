36600e4e9a0171ae4ecc10253d4e403031fea74a
aripineninavya-rgb <aripineninavya@gmail.com>
2026-02-12 18:10:33 +0100

    Update the search functionality to see the headline

--- index.ts
+++ index.ts
@@ -2,7 +2,8 @@ const jobsearch = async () => {
   try {
     const result = `https://jobsearch.api.jobtechdev.se/search?q=Software%20Developer&offset=0&limit=10`;
     const response = await fetch(result);
-    console.log(response);
+    const data = await response.json();
+    console.log(data);
   } catch (error) {
     console.error(error);
   }
